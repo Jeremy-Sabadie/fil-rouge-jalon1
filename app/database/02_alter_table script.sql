765e0a4503c880992c7ae288b1b657b95618d9ad
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2023-05-27 21:37:49 +0200

    littleModificationsOnForeignKeyInTheAlter script

--- app/database/02_alter_table script.sql
+++ app/database/02_alter_table script.sql
@@ -2,23 +2,23 @@ ALTER TABLE users add column
 pernom varchar(10),
 nom varchar(10);
 
-ALTER TABLE tickets add constrain
+ALTER TABLE tickets add constrain pk_tickets
  primary key(id);
 
 ALTER TABLE tickets add foreign key
 (id_auteur)reference users(id_user);
 
-ALTER TABLE message add foreign key
+ALTER TABLE message add foreign key fk_message
 (id_auteur)reference users(id);
 
-ALTER TABLE ticket_message add foreign key
+ALTER TABLE ticket_message add foreign key fk_ticket_message
 (id_ticket)reference tickets(id);
 
 ALTER TABLE ticket_message add constrain
- primary key(id);
+ pk_ticket_message primary key(id);
 
 ALTER TABLE ticket_materiel add constrain
  primary key(id);
 
  ALTER TABLE ticket_materiel add foreign key
-(id_materiel)reference idn(id);
+fk_ticket_materiel(id_materiel)reference idn(id);
