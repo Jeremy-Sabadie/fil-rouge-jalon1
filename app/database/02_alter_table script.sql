8ee7e8df09c88800eced8b2037765a14618dabc8
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2023-05-28 10:45:10 +0200

    alterTableScriptCorrected

--- app/database/02_alter_table script.sql
+++ app/database/02_alter_table script.sql
@@ -1,24 +1,23 @@
-ALTER TABLE users add column
-pernom varchar(10),
-nom varchar(10);
+ALTER TABLE users
+ADD COLUMN prenom varchar(10),
+ADD COLUMN nom varchar(10),
+ADD CONSTRAINT pk_users PRIMARY KEY (id_user);
 
-ALTER TABLE tickets add constrain pk_tickets
- primary key(id);
+ALTER TABLE tickets
+ADD CONSTRAINT pk_tickets PRIMARY KEY (id);
 
-ALTER TABLE tickets add foreign key
-(id_auteur)reference users(id_user);
+ALTER TABLE tickets
+ADD FOREIGN KEY (id_auteur) REFERENCES users(id_user);
 
-ALTER TABLE message add foreign key fk_message
-(id_auteur)reference users(id);
+ALTER TABLE message
+ADD CONSTRAINT fk_message FOREIGN KEY (id_auteur) REFERENCES users(id);
 
-ALTER TABLE ticket_message add foreign key fk_ticket_message
-(id_ticket)reference tickets(id);
+ALTER TABLE ticket_message
+ADD CONSTRAINT fk_ticket_message FOREIGN KEY (id_ticket) REFERENCES tickets(id);
 
-ALTER TABLE ticket_message add constrain
- pk_ticket_message primary key(id);
+ALTER TABLE ticket_message
+ADD CONSTRAINT pk_ticket_message PRIMARY KEY (id);
 
-ALTER TABLE ticket_materiel add constrain
- primary key(id);
+ALTER TABLE ticket_materiel
+ADD CONSTRAINT fk_ticket_materiel FOREIGN KEY (id_materiel) REFERENCES idn(id);
 
- ALTER TABLE ticket_materiel add foreign key
-fk_ticket_materiel(id_materiel)reference idn(id);
