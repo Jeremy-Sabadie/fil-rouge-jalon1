1e2840fdf1fcd2629c90043fbfcaed054f66e515
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2023-05-28 14:38:03 +0200

    alter_table_script_done

--- app/database/02_alter_table script.sql
+++ app/database/02_alter_table script.sql
@@ -1,16 +1,23 @@
 ALTER TABLE users
-ADD COLUMN prenom varchar(10),
-ADD COLUMN nom varchar(10),
-ADD CONSTRAINT pk_users PRIMARY KEY (id_user);
+DROP CONSTRAINT IF EXISTS pk_users;
 
-ALTER TABLE tickets
-ADD CONSTRAINT pk_tickets PRIMARY KEY (id);
+-- ALTER TABLE users
+-- ADD COLUMN prenom varchar(10),
+-- ADD COLUMN tel varchar(10);
 
-ALTER TABLE tickets
-ADD FOREIGN KEY (id_auteur) REFERENCES users(id_user);
 
-ALTER TABLE message
+-- ALTER TABLE ticket
+-- ADD
+--  FOREIGN KEY (id_auteur) REFERENCES users(id);
+
+-- ALTER TABLE messages
+-- ADD constraint pk_id_messsage PRIMARY key(id_message);
+
+ALTER TABLE messages
 ADD CONSTRAINT fk_message FOREIGN KEY (id_auteur) REFERENCES users(id);
+ALTER TABLE messages
+add
+ FOREIGN KEY (id_auteur) REFERENCES users(id);
 
 ALTER TABLE ticket_message
 ADD CONSTRAINT fk_ticket_message FOREIGN KEY (id_ticket) REFERENCES tickets(id);
@@ -20,4 +27,12 @@ ADD CONSTRAINT pk_ticket_message PRIMARY KEY (id);
 
 ALTER TABLE ticket_materiel
 ADD CONSTRAINT fk_ticket_materiel FOREIGN KEY (id_materiel) REFERENCES idn(id);
+
+ALTER TABLE ticket_message
+ADD  FOREIGN KEY (id_ticket) REFERENCES ticket(id);
+
+--généré avec l'nterface graphique de dbeaver:
+ALTER TABLE filrouge.ticket_message
+ADD CONSTRAINT ticket_message_FK_1
+FOREIGN KEY (id_message) REFERENCES filrouge.messages(id_message);
 
