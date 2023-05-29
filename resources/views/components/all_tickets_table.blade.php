1c1c0f0bf888852fe74a72f628a74ae6938fff94
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2023-05-29 18:07:39 +0200

    add two buttons under  table to go back on all.

--- resources/views/components/all_tickets_table.blade.php
+++ resources/views/components/all_tickets_table.blade.php
@@ -1,9 +1,7 @@
 @extends('templates.home')
 
 @section('content')
-    @if (session()->has('message'))
-        {{ session('message') }}
-    @endif
+
     <h3>Tous vos tickets</h3>
     <table>
         <thead>
@@ -20,17 +18,18 @@
                 <tr>
                     <td>{{ $ticket->id }}</td>
                     <td>{{ $ticket->sujet }}</td>
-                    {{-- <td>{{ $ticket->idstatus }}</td> --}}
-                    {{-- <td>{{ $ticket->cdat }}</td> --}}
-                    <td>
-
-                    </td>
                     <td><img src="/images/alerte.png" alt=""></td>
+                    <td>{{ $ticket->created_dat }}</td>
                     <td><button type="button"><a href="{{ route('ticket_detail', ['n' => $ticket->id]) }}">Detail</a></button></td>
+
+
+
+
+
                 </tr>
             @empty
                 <p>...</p>
             @endforelse
         </tbody>
     </table>
-@endsection
+    @endsection
