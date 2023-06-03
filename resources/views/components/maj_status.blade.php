8dcba80a9c4b97b04d084e373f5be7eb94d89400
Jeremy-Sabadie <sabadie.jeremy@gmail.com>
2023-06-03 08:49:17 +0200

    sttus update compomponent front.

---
+++ resources/views/components/maj_status.blade.php
@@ -0,0 +1,15 @@
+<fieldset>
+        <legend>mettre à jour le status du ticket n ...</legend>
+
+        <form action="" method="post">
+
+                <label for="no">ouvert</label>
+                <input type="radio" name="open" id="open" value="" checked="checked">
+
+                    <label for="done">résolu</label>
+                    <input type="radio" name="done" id="done">
+
+
+            <button type="submit">mettre à jour le status</button>
+        </form>
+    </fieldset>
